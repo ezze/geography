8106ec5cd249e233cf1a23786a1652962889f6a9
Dmitriy Pushkov <ezze@ezze.org>
2020-11-09 21:13:07 +0300

    South america challenge is created.

--- src/challenges/index.js
+++ src/challenges/index.js
@@ -1,11 +1,13 @@
 import russianRegions from './russian-regions/challenge.json';
 import europeCountries from './europe-countries/challenge.json';
 import asiaCountries from './asia-countries/challenge.json';
+import southAmericaCountries from './south-america-countries/challenge.json';
 import africaCountries from './africa-countries/challenge.json';
 
 export default [
   russianRegions,
   europeCountries,
   asiaCountries,
+  southAmericaCountries,
   africaCountries
 ];

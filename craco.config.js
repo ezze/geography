4eb26f916acb025df5d2d5af65197623465f8a18
Dmitriy Pushkov <ezze@ezze.org>
2024-09-14 02:00:44 +0300

    Ignore source map parsing warnings

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+module.exports = {
+  webpack: {
+    configure: {
+      ignoreWarnings: [
+        function (warning) {
+          return (
+            warning.module &&
+            warning.module.resource.includes('node_modules') &&
+            warning.details &&
+            warning.details.includes('source-map-loader')
+          );
+        }
+      ]
+    }
+  }
+};
